af79b25dcb02fffdc0e26d259c69d96c72819623
qiqinb <110291521+qiqizyc@users.noreply.github.com>
2026-01-08 22:04:27 +0800

    Update footer year and remove CDN link

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="background"></div>
     <Home />
   <footer>
-    <span>© 2025 Made By <a href="/" target="_blank">{{ userName }}</a> | 本站由<a target="_blank" href="https://cloud.baidu.com/product/cdn.html">百度云CDN</a>
-      、<a target="_blank" href="https://www.aliyun.com/product/cdn">阿里云CDN</a>提供加速 | </span>
+    <span>© 2026 Made By <a href="/" target="_blank">{{ userName }}</a> | 本站由<a target="_blank" href="https://cloud.baidu.com/product/cdn.html">百度云CDN</a>提供加速 | </span>
+    <!--、<a target="_blank" href="https://www.aliyun.com/product/cdn">阿里云CDN</a>-->
     <a v-if="icpNumber" href="https://beian.miit.gov.cn/" target="_blank">{{ icpNumber }}</a>
     <a v-if="policenumber" :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`" target="_blank" class="police_link">
       <span class="police_img"></span> {{ policenumber }}
